804e11ad985ceb79ce2748ab31b3266f6672a314
Dídac Padilla <didacpadilla20@gmail.com>
2019-12-02 15:03:27 +0100

    Neighbour done

---
+++ Project/Assets/Scripts/QuestSystem/Q0/RadioQuest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioQuest : InteractableQuest
+{
+
+    protected override void Start()
+    {
+        base.Start();
+        gc.audioManager.PlayAtPosition("Radio-Song", transform);
+    }
+
+}
